19251fa91adcf00199e124673b090a534554354a
mohammad sw-hx <178472433+sw-hx@users.noreply.github.com>
2026-03-11 12:48:04 +0300

    some refactoring

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import authMiddleware from "./middleware/authMiddleware.js";
 import trimMiddleware from "./middleware/trimMiddleware.js";
 import authRoutes from "./routes/authRoutes.js";
 import categoryRoutes from "./routes/categories.js";
+import serviceRoutes from "./routes/services.js";
 import testDatabaseConnection from "./testing/database/connectToDatabase.js";
 import models from "./models/index.js";
 
@@ -22,6 +23,7 @@ app.use(trimMiddleware);
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/categories", authMiddleware, categoryRoutes);
+app.use("/api/services", authMiddleware, serviceRoutes);
 
 app.listen(PORT, () => {
   console.log(`node is running at PORT ${PORT}`);
